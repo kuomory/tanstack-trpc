b38eaf61e5b336fd29197a3c85f60fd4765533dc
h_horiike <h_horiike@sbs-infosys.co.jp>
2024-08-06 08:01:03 +0000

    localhost 127.0.0.1対応

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -13,7 +13,7 @@ const server = fastify({
 });
 
 server.register(cors, {
-  origin: "http://localhost:5173",
+  origin: "http://127.0.0.1:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 });
--- services/web/vite.config.ts
+++ services/web/vite.config.ts
@@ -5,4 +5,8 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+  },
 });
